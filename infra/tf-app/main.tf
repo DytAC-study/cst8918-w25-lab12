b774387e8cf6898619c2afcf168fde65dc823dda
zhao0294 <zhao0294@algonquinlive.com>
2025-07-28 15:10:33 -0400

    Add tags to trigger workflow

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,4 +1,8 @@
 resource "azurerm_resource_group" "app_rg" {
   name     = "0411175585-a12-rg"
   location = "canadaeast"
+  tags = {
+    Environment = "production"
+    Project     = "cst8918-lab12"
+  }
 }
